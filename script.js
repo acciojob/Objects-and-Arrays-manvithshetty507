effda79f4989bf577f1b2b87a42a0c64f578f989
Manvith Kumar shetty <121666002+manvithshetty507@users.noreply.github.com>
2023-06-09 09:58:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,11 @@ const person = {
 };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
-let team = players;
+let team =[];
+let n = players.length;
+for(let i=0;i<n;i++){
+	team.push[i]=i;
+}
 let team1 = [];
 
 for(player of players){
